74685d8aff5b1e9cda457cee2b848d4f0c8bff9e
Karsten Panier <karsten.panier@signal-iduna.de>
2018-04-21 17:59:35 +0200

    Add helo world as server response

--- src/API.ts
+++ src/API.ts
@@ -1,4 +1,6 @@
 import express = require('express');
+import { ResponseBody, OutputSpeech } from 'alexa-sdk';
+import { Response } from 'alexa-sdk';
 
 export class API {
 
@@ -28,7 +30,7 @@ export class API {
         let httpsServer = https.createServer(credentials, this.server);
         httpsServer.listen(443);
     }
-    
+
     createHelloMessageEndpoint(): any {
         return (request, response) => {
             response.send('Server is up and running.');
@@ -37,8 +39,29 @@ export class API {
 
     createAlexaEndpoint(): any {
         return (request, response) => {
-            console.log(request.body);
-            response.status(200);
+            let alexaReq = JSON.parse(request.body)
+            console.log(alexaReq);
+            let reqType = alexaReq.request.type;
+            console.log(reqType)
+            let result = {};
+            switch (reqType) {
+                case 'LaunchRequest':
+                    response.status(200);
+                    result = this.getHelloResponse();
+                    break;
+                default:
+                    response.status(200);
+            }
+            response.send(result);
+        }
+    }
+
+    getHelloResponse(): ResponseBody {
+        let speech: OutputSpeech = {text: "Hallo von Kater Lee", type: "PlainText"};
+        let res: Response = {outputSpeech: speech};
+        return {
+            version: "1.0",
+            response: res
         }
     }
 
